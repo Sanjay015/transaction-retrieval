6a27b06b5386c8f383aaa5195343eee08e60b9f5
Sanjay Yadav <sanjay.cc.in@gmail.com>
2019-07-13 21:37:04 +0530

    ADD: Added summary view by product and city

--- summary/urls.py
+++ summary/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
-from .views import transaction_data, load_reference_data
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    path('load_data/', transaction_data, name='load-data'),
+    path('load_data/', views.transaction_data, name='load-data'),
+
+    url(r'^transactionSummary/(?P<transaction_id>[0-9]+)/$',
+        views.transaction_summary, name='transaction-summary'),
+
+    url(r'^transactionSummaryByProducts/(?P<last_n_days>[0-9]+)/$',
+        views.transaction_summary_by_product, name='transaction-summary-by-product'),
+
+    url(r'^transactionSummaryByManufacturingCity/(?P<last_n_days>[0-9]+)/$',
+        views.transaction_summary_by_manufacturing_city, name='transaction-summary-by-city'),
 ]
+
 # Load reference data on server startup
-load_reference_data()
+views.load_reference_data()
 # Load transaction data on server startup
-transaction_data(None)
+views.transaction_data(None)
--- summary/views.py
+++ summary/views.py
@@ -12,6 +12,18 @@ logger = logging.getLogger("app")
 DATA = {}
 
 
+def calculate_last_n_days(last_n_days: int) -> datetime:
+    """Calculate date interval
+        Args:
+            last_n_days: Go back to number of days from current date
+        Returns:
+            starting date(which is n days back from current date)
+    """
+    today = datetime.datetime.now().replace(minute=0, hour=0, second=0, microsecond=0)
+    from_date = today - datetime.timedelta(days=last_n_days)
+    return from_date
+
+
 def load_reference_data():
     """Load and cache reference data"""
     reference_file = os.path.join(get_root(), config.REFERENCE_DATA_FILE)
@@ -29,11 +41,17 @@ def load_reference_data():
 
 def transaction_data(request):
     """Load transaction data"""
+    # Load reference mapping
+    load_reference_data()
+
     # Get transaction files to load
     trans_files = transaction_files(load_processed=True if "transaction_data" not in DATA else False)
     for trans_file in trans_files:
         if os.path.getsize(trans_file) > 0:
             data = pd.read_csv(trans_file, parse_dates=[config.TRANS_DATE_COL])
+            # Adding product mapping to transaction data
+            data = pd.merge(data, DATA["reference_data"], on=["productId"], how="left")
+
             DATA["transaction_data"] = data if "transaction_data" not in DATA else DATA["transaction_data"].append(data)
             logger.debug("Data loaded from file: {}".format(trans_file))
         else:
@@ -41,3 +59,40 @@ def transaction_data(request):
         move_transaction_file(trans_file)
         logger.debug("Moved file from : {} to processed location".format(trans_file))
     return JsonResponse({"status": "Data loaded successfully"})
+
+
+def transaction_summary(request, transaction_id):
+    """Generate full detail of transaction"""
+    data = DATA["transaction_data"][DATA["transaction_data"]["transactionId"] == int(transaction_id)]
+    if not len(data):
+        return JsonResponse({"data": "No records found"})
+
+    data = data[["transactionId", "productName", "transactionAmount", "transactionDatetime"]]
+    data["transactionDatetime"] = data["transactionDatetime"].apply(lambda x: x.strftime("%Y-%m-%d %H:%M:%S"))
+    return JsonResponse({"data": data.to_dict(orient="records")})
+
+
+def transaction_summary_by_product(request, last_n_days):
+    """Get transaction summary by products in last n days from current day"""
+    from_date = calculate_last_n_days(int(last_n_days))
+    data = DATA["transaction_data"][DATA["transaction_data"][config.TRANS_DATE_COL] >= from_date]
+    if not len(data):
+        return JsonResponse({"summary": "No records found"})
+
+    data = data[["productName", "transactionAmount"]]
+    data = data.rename(columns={"transactionAmount": "totalAmount"})
+    data = data.groupby(["productName"], as_index=False).agg({"totalAmount": sum})
+    return JsonResponse({"summary": data.to_dict(orient="records")})
+
+
+def transaction_summary_by_manufacturing_city(request, last_n_days):
+    """Get transaction summary by manufacturing city in last n days from current day"""
+    from_date = calculate_last_n_days(int(last_n_days))
+    data = DATA["transaction_data"][DATA["transaction_data"][config.TRANS_DATE_COL] >= from_date]
+    if not len(data):
+        return JsonResponse({"summary": "No records found"})
+
+    data = data[["productManufacturingCity", "transactionAmount"]]
+    data = data.rename(columns={"productManufacturingCity": "cityName", "transactionAmount": "totalAmount"})
+    data = data.groupby(["cityName"], as_index=False).agg({"totalAmount": sum})
+    return JsonResponse({"summary": data.to_dict(orient="records")})
